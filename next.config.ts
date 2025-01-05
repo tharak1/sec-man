3f16c3822783b81ee5b5dca674a55f0910852699
tharak1 <saitharakreddyv59@gmail.com>
2025-01-05 07:42:05 +0530

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,11 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
 }
 
 export default nextConfig;
